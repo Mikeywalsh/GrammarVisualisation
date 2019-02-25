077ed8a5e9b90d1655b79c2cff2eb5db2b6f4e65
Michael Walsh <Michaelwalsh223@hotmail.co.uk>
2019-02-25 23:46:34 +0000

    Documented GrammarNodeType

--- Assets/Code/Enums/GrammarNodeType.cs
+++ Assets/Code/Enums/GrammarNodeType.cs
@@ -1,4 +1,7 @@
-﻿public enum GrammarNodeType
+﻿/// <summary>
+/// Represents the different types of grammar nodes that can exist
+/// </summary>
+public enum GrammarNodeType
 {
     NONTERMINAL = 0,
     TERMINAL,
