fcfbc7488b892b388511bcfbf7e4d2d869d40b39
Michael Walsh <michaelwalsh223@hotmail.co.uk>
2019-03-01 15:48:23 +0000

    Added iTween to unity project

--- UnityVisualisation/Assets/Code/LineConnection.cs
+++ UnityVisualisation/Assets/Code/LineConnection.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-sealed public class LineConnection : MonoBehaviour
+public sealed class LineConnection : MonoBehaviour
 {
     private LineRenderer line;
     private GameObject Target;
@@ -15,6 +15,10 @@ sealed public class LineConnection : MonoBehaviour
         Vector3[] linePositions = new Vector3[2];
         linePositions[0] = transform.position;
         linePositions[1] = Target?.transform.position ?? transform.position;
+
+        // Put lines behind nodes
+        linePositions[0] += new Vector3(0, 0, 1);
+        linePositions[1] += new Vector3(0, 0, 1);
 
         line.SetPositions(linePositions);
     }
