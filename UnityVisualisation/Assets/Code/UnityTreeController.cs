fa3b8b62c3158c912651d8e8a00e6588921e7095
Michael Walsh <michaelwalsh223@hotmail.co.uk>
2019-03-01 17:23:05 +0000

    Animation, because why not

--- UnityVisualisation/Assets/Code/UnityTreeController.cs
+++ UnityVisualisation/Assets/Code/UnityTreeController.cs
@@ -7,20 +7,19 @@ using UnityEngine;
 
 public class UnityTreeController : MonoBehaviour
 {
-	public GrammarTree CurrentTree;
+    public GrammarTree CurrentTree;
 
     public const float H_SPACING = 1;
     public const float V_SPACING = 6;
-    
+
     public const float NODE_WIDTH = 7;
     public const float NODE_HEIGHT = 3;
 
     public Dictionary<TreeNode<GrammarData>, GameObject> nodeToObjectMap = new Dictionary<TreeNode<GrammarData>, GameObject>();
 
-
     private void Start()
     {
-		CurrentTree = new GrammarTree("Assets/sampleGrammar.txt", new Vector2D(H_SPACING, V_SPACING), new Vector2D(NODE_WIDTH, NODE_HEIGHT));
+        CurrentTree = new GrammarTree("Assets/sampleGrammar.txt", new Vector2D(H_SPACING, V_SPACING), new Vector2D(NODE_WIDTH, NODE_HEIGHT));
 
         // Create the root node as a GameObject and add it to the node To Object map
         CreateNodeObjectForNode(CurrentTree.Root);
@@ -37,6 +36,50 @@ public class UnityTreeController : MonoBehaviour
 
         // Initialise the UI
         UIController.Controller.InitialiseUI();
+
+        // Hide all Node Objects
+        foreach (var nodeObject in nodeToObjectMap.Values)
+        {
+            nodeObject.SetActive(false);
+        }
+
+        SpawnNextLayer();
+    }
+
+    private int currentSpawningDepth = 0;
+    private int nodesThisDepth;
+    private int spawnsThisDepth;
+
+    public void SpawnNextLayer()
+    {
+        // Get all gameobjects that represent treenodes at the next layer
+        var treeNodesToSpawn = nodeToObjectMap.Keys.Where(n => n.Depth == currentSpawningDepth).ToList();
+
+        nodesThisDepth = treeNodesToSpawn.Count;
+        spawnsThisDepth = 0;
+
+        foreach (var node in treeNodesToSpawn)
+        {
+            var nodeObject = nodeToObjectMap[node];
+            nodeObject.SetActive(true);
+            if (node.Parent != null)
+            {
+                nodeObject.transform.position = new Vector3(node.Parent.Position.X, node.Parent.Position.Y, 0);
+            }
+
+            iTween.MoveTo(nodeObject, iTween.Hash("x", node.Position.X, "y", node.Position.Y, "time", 0.25f, "onCompleteTarget", gameObject, "onComplete", "SpawnComplete"));
+        }
+    }
+
+    public void SpawnComplete()
+    {
+        spawnsThisDepth++;
+
+        if (nodesThisDepth == spawnsThisDepth)
+        {
+            SpawnNextLayer();
+            currentSpawningDepth++;
+        }
     }
 
     private Rect CalculateTreeBounds()
@@ -54,7 +97,7 @@ public class UnityTreeController : MonoBehaviour
         GameObject newNodeObject = Instantiate(Resources.Load("Grammar Node"), Vector3.zero, Quaternion.identity) as GameObject;
         newNodeObject.GetComponent<UnityTreeNodeObject>().SetNode(node);
         nodeToObjectMap.Add(node, newNodeObject);
-		newNodeObject.GetComponent<UnityTreeNodeObject>().PositionNode();
+        newNodeObject.GetComponent<UnityTreeNodeObject>().PositionNode();
     }
 
     private void CreateChildrenObjects(TreeNode<GrammarData> node)
@@ -62,16 +105,16 @@ public class UnityTreeController : MonoBehaviour
         // Create GameObjects for each child
         foreach (var treeNode in node.Children)
         {
-	        var child = treeNode;
-	        CreateNodeObjectForNode(child);
+            var child = treeNode;
+            CreateNodeObjectForNode(child);
             CreateChildrenObjects(child);
         }
     }
 
-	/// <summary>
-	/// For each node in the tree, create a line connection between it and its parent, if it has one
-	/// </summary>
-	public void CreateLineConnections()
+    /// <summary>
+    /// For each node in the tree, create a line connection between it and its parent, if it has one
+    /// </summary>
+    public void CreateLineConnections()
     {
         foreach (var node in CurrentTree.AllNodes)
         {
